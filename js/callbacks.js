544cc007ae24bc75cd51a635baf699a171eb0ae6
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-05 16:02:20 -0700

    Implement a callback. Make another AJAX call to the Wikipedia API. Dynamically display the astronauts on the web page when the user clicks the button. Remove button when astronauts are displayed.

--- js/callbacks.js
+++ js/callbacks.js
@@ -4,13 +4,13 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = () => {
     if(xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
@@ -34,4 +34,11 @@ btn.addEventListener('click', () => getJSON(astrosUrl));
 getJSON(astrosUrl);
 
 //Create an event listener for the button to invoke the getJSON function
-btn.addEventListener('click', () => getJSON(astrosUrl));
+btn.addEventListener('click', (event) => {
+  getJSON(astrosUrl, (json) => {
+    json.people.map( person => {
+      getJSON(wikiUrl + person.name, generateHTML);
+    });
+  });
+  event.target.remove();
+});
