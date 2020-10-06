f2a1645d03c92953f8036ac7cf35ef6238da77cb
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-06 08:12:00 -0700

    practice creating a promise.

---
+++ js/promises-breakfast.js
@@ -0,0 +1,8 @@
+const breakfastPromise = new Promise( (resolve, reject) => {
+  setTimeout(() => {
+    resolve('Your order is ready. Come and get it!');
+  }, 3000);
+});
+
+console.log(breakfastPromise);
+breakfastPromise.then( val => console.log(val) );
