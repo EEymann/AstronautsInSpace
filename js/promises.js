980215ed8537c7d3563b2860c433c1c1a301a70f
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-07 10:59:33 -0700

    Handle Multiple Promises with Promis.all.

--- js/promises.js
+++ js/promises.js
@@ -25,24 +25,26 @@ function getProfiles(json) {
   const profiles = json.people.map( person => {
     return getJSON(wikiUrl + person.name);
   });
-  return profiles;
+  return Promise.all(profiles); //Promise.all waits on all the individual promise objects then joins them into one and returns a value when all specified promises are fulfilled.
 }
 
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  section.innerHTML = `
-    <img src=${data.thumbnail.source}>
-    <h2>${data.title}</h2>
-    <p>${data.description}</p>
-    <p>${data.extract}</p>
-  `;
+  data.map( person => {
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    section.innerHTML = `
+      <img src=${person.thumbnail.source}>
+      <h2>${person.title}</h2>
+      <p>${person.description}</p>
+      <p>${person.extract}</p>
+    `;
+  });
 }
 
 btn.addEventListener('click', (event) => {
   getJSON(astrosUrl)
     .then(getProfiles)
-    .then( data => console.log(data) )
+    .then(generateHTML)
     .catch( err => console.log(err) )
   event.target.remove()
 });
