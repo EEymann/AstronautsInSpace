7654cb80fbcab2731946126d48929e7b79caafc5
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-08 13:42:30 -0700

    convert promise handling into async/await.

--- js/async-await.js
+++ js/async-await.js
@@ -4,7 +4,27 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+// This function is going to first make a fetch request to the open notify API,
+// then use those results to make fetch requests to the Wikipedia API.
+//The function takes the parameter URL, for the url to fetch.
+//In the body of the function, we'll start by making the first network request using the fetch method.
+//It will be to the open notify API to get the names of the people in space.
+//Fetch returns a promise. The await keyword waits for a resolved promise returned by fetch.
+//Then it's going to get the fulfillment value out of the promise and assign it to peopleResponse.
+// const peopleJSON = peopleResponse.json();  parses the response from Fetch to JSON.
+async function getPeopleInSpace(url) { //fetches url
+  const peopleResponse = await fetch(url); //await the response,
+  const peopleJSON = await peopleResponse.json(); //read the response and await the JSON.
 
+  const profiles = peopleJSON.people.map( async person => {
+    const craft = person.craft;
+    const profileResponse = await fetch(wikiUrl + person.name);
+    const profileJSON = await profileResponse.json();
+
+    return { ...profileJSON, craft};
+  });
+    return Promise.all(profiles);
+}
 
 // Generate the markup for each profile
 function generateHTML(data) {
@@ -21,7 +41,10 @@ function generateHTML(data) {
   });
 }
 
-btn.addEventListener('click', (event) => {
+btn.addEventListener('click', async (event) => {
   event.target.textContent = "Loading...";
 
+  const astros = await getPeopleInSpace(astrosUrl);
+  generateHTML(astros);
+  event.target.remove();
 });
