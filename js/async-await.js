0705219615cdddb37767d248f3285aff71a9eb92
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-08 16:27:40 -0700

    Error Handling with try...catch. Fixbug.

--- js/async-await.js
+++ js/async-await.js
@@ -4,32 +4,29 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
-  async function getJSON(url) {
-    try {
-      const response = await fetch(url);
-      return await response.json();
-    } catch (error) {
-      throw error;
-    }
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
   }
-
+}
 
 async function getPeopleInSpace(url) {
   const peopleJSON = await getJSON(url);
 
   const profiles = peopleJSON.people.map( async (person) => {
-  const craft = person.craft;
-  const profileJSON = await getJSON(wikiUrl + person.name);
+    const craft = person.craft;
+    const profileJSON = await getJSON(wikiUrl + person.name);
 
     return { ...profileJSON, craft };
   });
-    return Promise.all(profiles);
+
+  return Promise.all(profiles);
 }
 
-
-
 // Generate the markup for each profile
-
 function generateHTML(data) {
   data.map( person => {
     const section = document.createElement('section');
@@ -48,11 +45,10 @@ btn.addEventListener('click', (event) => {
   event.target.textContent = 'Loading...';
 
   getPeopleInSpace(astrosUrl)
-  .then(generateHTML)
-  .catch( e => {
-    peopleList.innerHTML = '<h3>Something went wrong!</h3>';
-    console.error(e);
-  })
-  .finally( () => event.target.remove() )
-
+    .then(generateHTML)
+    .catch( e => {
+      peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+      console.error(e);
+    })
+    .finally( () => event.target.remove() )
 });
